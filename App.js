c967a189dea32acca1b30cffd1ef5874b351c4fb
kyhyeok <kyhyeok@gmail.com>
2020-10-20 14:48:18 +0900

    Custom Fonts

--- App.js
+++ App.js
@@ -1,74 +1,24 @@
 import React, { useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  FlatList,
-  Alert,
-  TouchableWithoutFeedback,
-  Keyboard
-} from "react-native";
+import * as Font from "expo-font";
+import Home from "./screens/home";
+import About from "./screens/about";
+import ReviewDetail from "./screens/reviewDetail";
 
-import Header from "./components/Header";
-import ToDoItem from "./components/ToDoItem";
-import AddToDo from "./components/AddToDo";
+import { AppLoading } from "expo";
+
+const getFonts = () =>
+  Font.loadAsync({
+    "nunito-regular": require("./assets/fonts/Nunito-Regular.ttf"),
+    "nunito-bold": require("./assets/fonts/Nunito-Bold.ttf")
+  });
 
 export default function App() {
-  const [toDos, setToDos] = useState([
-    { text: "buy coffee", key: "1" },
-    { text: "create an app", key: "2" },
-    { text: "play on the switch", key: "3" }
-  ]);
-
-  const pressHandler = (key) => {
-    setToDos((prevToDos) => {
-      return prevToDos.filter((todo) => todo.key !== key);
-    });
-  };
-
-  const submitHandler = (text) => {
-    if (text.length > 3) {
-      setToDos((prevToDos) => {
-        return [{ text, key: Math.random().toString() }, ...prevToDos];
-      });
-    } else {
-      Alert.alert("OOPS!!", "toDos must be over 3 chars long", [
-        { text: "Understood", onPress: () => console.log("alert closed") }
-      ]);
-    }
-  };
-
-  return (
-    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-      <View style={styles.container}>
-        <Header />
-        <View style={styles.content}>
-          <AddToDo submitHandler={submitHandler} />
-          <View style={styles.list}>
-            <FlatList
-              data={toDos}
-              renderItem={({ item }) => (
-                <ToDoItem item={item} pressHandler={pressHandler} />
-              )}
-            />
-          </View>
-        </View>
-      </View>
-    </TouchableWithoutFeedback>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff"
-  },
-  content: {
-    padding: 40,
-    flex: 1
-  },
-  list: {
-    marginTop: 20,
-    flex: 1
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+  if (fontsLoaded) {
+    return <Home />;
+  } else {
+    return (
+      <AppLoading startAsync={getFonts} onFinish={() => setFontsLoaded(true)} />
+    );
   }
-});
+}
