31902d260ed25be79eb7710c3a508980327a1c16
kyhyeok <kyhyeok@gmail.com>
2020-10-20 18:21:02 +0900

    Navigating Around

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,11 +1,15 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View, Button } from "react-native";
 import { globalStyles } from "../styles/global";
 
-export default function ReviewDetails() {
+export default function ReviewDetails({ navigation }) {
+  const pressHandler = () => {
+    navigation.goBack();
+  };
   return (
     <View style={globalStyles.container}>
       <Text>reviewDetail Screen</Text>
+      <Button title="back to home screen" onPress={pressHandler} />
     </View>
   );
 }
